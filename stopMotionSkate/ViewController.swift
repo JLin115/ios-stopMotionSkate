20c97eafee173033e2da7c1e62fca9aa48e95207
林哲右 <jamesbuy218@yahoo.com.tw>
2018-07-21 00:55:03 -0700

    Initial Commit

---
+++ stopMotionSkate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stopMotionSkate
+//
+//  Created by 林哲右 on 2018/7/21.
+//  Copyright © 2018年 stopMotionSkate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
